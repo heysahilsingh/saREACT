a8d974bb0cf705de24685c0d14d7d9c95862c72c
Sahil <heysahilbro@gmail.com>
2023-08-01 18:42:34 +0530

    Aug 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './main.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('main')!).render(
   <React.StrictMode>
-    <App />
+    <App h1 = "New Heading" defaultClass='default class sa' p = "This is a paragraph for this page. Just a dummy one." />
   </React.StrictMode>,
 )
