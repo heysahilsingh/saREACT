006760801f13711d5f03e969d3c6c795f045b13d
Sahil <heysahilbro@gmail.com>
2023-08-24 19:00:41 +0530

    c

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,20 @@
 export const imageUrl = "../media/images";
+
+export const API_URL = {
+    
+}
+
+// ROUTES
+/*
+
+1. https://www.zee5.com/movies/details/mrs-undercover/0-0-1z5334341
+   mainURL: https://www.zee5.com/movies/details
+   editable: /mrs-undercover
+   IDofDetailer: /0-0-1z5334341
+
+
+
+
+*/
+
+// APIs
