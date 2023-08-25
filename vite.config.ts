f4ea13d0eefde2ba0d2a45fd66979c05f10004d1
Sahil <heysahilbro@gmail.com>
2023-08-25 14:19:24 +0530

    deployement setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/swiggy/',
+  base: './',
   root: "./src",
   build: {
     outDir: '../dist'
